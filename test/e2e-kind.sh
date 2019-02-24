f451b4e36c00194feccdac5abff372fb1415ec33
stefanprodan <stefan.prodan@gmail.com>
2019-02-24 02:52:25 +0200

    Split e2e prerequisites

--- test/e2e-kind.sh
+++ test/e2e-kind.sh
@@ -21,11 +21,5 @@ sudo mv kind /usr/local/bin/
 kind create cluster --wait 5m
 
 export KUBECONFIG="$(kind get kubeconfig-path --name="kind")"
+kubectl get pods --all-namespaces
 
-echo ">>> Installing helm"
-curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
-
-echo '>>> Installing tiller'
-kubectl --namespace kube-system create sa tiller
-kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-helm init --service-account tiller --upgrade --wait
--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -2,24 +2,8 @@
 
 set -o errexit
 
-ISTIO_VER="1.1.0-rc.0"
 REPO_ROOT=$(git rev-parse --show-toplevel)
 export KUBECONFIG="$(kind get kubeconfig-path --name="kind")"
-
-echo ">>> Starting e2e testing using Istio ${ISTIO_VER}"
-helm repo add istio.io https://storage.googleapis.com/istio-release/releases/${ISTIO_VER}/charts
-
-echo '>>> Installing Istio CRDs'
-helm upgrade -i istio-init istio.io/istio-init --wait --namespace istio-system
-
-echo '>>> Waiting for Istio CRDs to be ready'
-kubectl -n istio-system wait --for=condition=complete job/istio-init-crd-10
-kubectl -n istio-system wait --for=condition=complete job/istio-init-crd-11
-
-echo 'Installing Istio control plane'
-helm upgrade -i istio istio.io/istio --wait --namespace istio-system -f ${REPO_ROOT}/test/e2e-istio-values.yaml
-
-export KUBECONFIG="$(kind get kubeconfig-path)"
 
 echo '>>> Building Flagger'
 cd ${REPO_ROOT} && docker build -t test/flagger:latest . -f Dockerfile
@@ -56,8 +40,8 @@ spec:
   service:
     port: 9898
   canaryAnalysis:
-    interval: 10s
-    threshold: 10
+    interval: 15s
+    threshold: 15
     maxWeight: 50
     stepWeight: 10
     metrics:
@@ -85,6 +69,7 @@ until ${ok}; do
     sleep 5
     count=$(($count + 1))
     if [[ ${count} -eq ${retries} ]]; then
+        kubectl -n istio-system logs deployment/flagger
         echo "No more retries left"
         exit 1
     fi
@@ -104,9 +89,14 @@ until ${ok}; do
     sleep 5
     count=$(($count + 1))
     if [[ ${count} -eq ${retries} ]]; then
+        kubectl -n istio-system logs deployment/flagger
         echo "No more retries left"
         exit 1
     fi
 done
 
 echo '>>> Canary promotion test passed ✔︎'
+
+kubectl -n istio-system logs deployment/flagger
+
+echo '>>> All tests passed ✔︎'
