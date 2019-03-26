ddd3a8251edd56b8db69441397143b8439ae78ce
stefanprodan <stefan.prodan@gmail.com>
2019-03-26 15:57:53 +0200

    Reconcile ClusterIP services - add svc update tests fix: #114

--- pkg/router/kubernetes.go
+++ pkg/router/kubernetes.go
@@ -2,7 +2,7 @@ package router
 
 import (
 	"fmt"
-
+	"github.com/google/go-cmp/cmp"
 	flaggerv1 "github.com/weaveworks/flagger/pkg/apis/flagger/v1alpha3"
 	clientset "github.com/weaveworks/flagger/pkg/client/clientset/versioned"
 	"go.uber.org/zap"
@@ -22,128 +22,27 @@ type KubernetesRouter struct {
 }
 
 // Sync creates or updates the primary and canary services
-func (c *KubernetesRouter) Sync(cd *flaggerv1.Canary) error {
-	targetName := cd.Spec.TargetRef.Name
+func (c *KubernetesRouter) Sync(canary *flaggerv1.Canary) error {
+	targetName := canary.Spec.TargetRef.Name
 	primaryName := fmt.Sprintf("%s-primary", targetName)
-	portName := cd.Spec.Service.PortName
-	if portName == "" {
-		portName = "http"
+	canaryName := fmt.Sprintf("%s-canary", targetName)
+
+	// main svc
+	err := c.syncService(canary, targetName, primaryName)
+	if err != nil {
+		return err
 	}
 
-	canaryService, err := c.kubeClient.CoreV1().Services(cd.Namespace).Get(targetName, metav1.GetOptions{})
-	if errors.IsNotFound(err) {
-		canaryService = &corev1.Service{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      targetName,
-				Namespace: cd.Namespace,
-				OwnerReferences: []metav1.OwnerReference{
-					*metav1.NewControllerRef(cd, schema.GroupVersionKind{
-						Group:   flaggerv1.SchemeGroupVersion.Group,
-						Version: flaggerv1.SchemeGroupVersion.Version,
-						Kind:    flaggerv1.CanaryKind,
-					}),
-				},
-			},
-			Spec: corev1.ServiceSpec{
-				Type:     corev1.ServiceTypeClusterIP,
-				Selector: map[string]string{"app": primaryName},
-				Ports: []corev1.ServicePort{
-					{
-						Name:     portName,
-						Protocol: corev1.ProtocolTCP,
-						Port:     cd.Spec.Service.Port,
-						TargetPort: intstr.IntOrString{
-							Type:   intstr.Int,
-							IntVal: cd.Spec.Service.Port,
-						},
-					},
-				},
-			},
-		}
-
-		_, err = c.kubeClient.CoreV1().Services(cd.Namespace).Create(canaryService)
-		if err != nil {
-			return err
-		}
-		c.logger.With("canary", fmt.Sprintf("%s.%s", cd.Name, cd.Namespace)).Infof("Service %s.%s created", canaryService.GetName(), cd.Namespace)
+	// canary svc
+	err = c.syncService(canary, canaryName, targetName)
+	if err != nil {
+		return err
 	}
 
-	canaryTestServiceName := fmt.Sprintf("%s-canary", cd.Spec.TargetRef.Name)
-	canaryTestService, err := c.kubeClient.CoreV1().Services(cd.Namespace).Get(canaryTestServiceName, metav1.GetOptions{})
-	if errors.IsNotFound(err) {
-		canaryTestService = &corev1.Service{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      canaryTestServiceName,
-				Namespace: cd.Namespace,
-				OwnerReferences: []metav1.OwnerReference{
-					*metav1.NewControllerRef(cd, schema.GroupVersionKind{
-						Group:   flaggerv1.SchemeGroupVersion.Group,
-						Version: flaggerv1.SchemeGroupVersion.Version,
-						Kind:    flaggerv1.CanaryKind,
-					}),
-				},
-			},
-			Spec: corev1.ServiceSpec{
-				Type:     corev1.ServiceTypeClusterIP,
-				Selector: map[string]string{"app": targetName},
-				Ports: []corev1.ServicePort{
-					{
-						Name:     portName,
-						Protocol: corev1.ProtocolTCP,
-						Port:     cd.Spec.Service.Port,
-						TargetPort: intstr.IntOrString{
-							Type:   intstr.Int,
-							IntVal: cd.Spec.Service.Port,
-						},
-					},
-				},
-			},
-		}
-
-		_, err = c.kubeClient.CoreV1().Services(cd.Namespace).Create(canaryTestService)
-		if err != nil {
-			return err
-		}
-		c.logger.With("canary", fmt.Sprintf("%s.%s", cd.Name, cd.Namespace)).Infof("Service %s.%s created", canaryTestService.GetName(), cd.Namespace)
-	}
-
-	primaryService, err := c.kubeClient.CoreV1().Services(cd.Namespace).Get(primaryName, metav1.GetOptions{})
-	if errors.IsNotFound(err) {
-		primaryService = &corev1.Service{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      primaryName,
-				Namespace: cd.Namespace,
-				OwnerReferences: []metav1.OwnerReference{
-					*metav1.NewControllerRef(cd, schema.GroupVersionKind{
-						Group:   flaggerv1.SchemeGroupVersion.Group,
-						Version: flaggerv1.SchemeGroupVersion.Version,
-						Kind:    flaggerv1.CanaryKind,
-					}),
-				},
-			},
-			Spec: corev1.ServiceSpec{
-				Type:     corev1.ServiceTypeClusterIP,
-				Selector: map[string]string{"app": primaryName},
-				Ports: []corev1.ServicePort{
-					{
-						Name:     portName,
-						Protocol: corev1.ProtocolTCP,
-						Port:     cd.Spec.Service.Port,
-						TargetPort: intstr.IntOrString{
-							Type:   intstr.Int,
-							IntVal: cd.Spec.Service.Port,
-						},
-					},
-				},
-			},
-		}
-
-		_, err = c.kubeClient.CoreV1().Services(cd.Namespace).Create(primaryService)
-		if err != nil {
-			return err
-		}
-
-		c.logger.With("canary", fmt.Sprintf("%s.%s", cd.Name, cd.Namespace)).Infof("Service %s.%s created", primaryService.GetName(), cd.Namespace)
+	// primary svc
+	err = c.syncService(canary, primaryName, primaryName)
+	if err != nil {
+		return err
 	}
 
 	return nil
@@ -155,4 +54,73 @@ func (c *KubernetesRouter) SetRoutes(canary *flaggerv1.Canary, primaryRoute int,
 
 func (c *KubernetesRouter) GetRoutes(canary *flaggerv1.Canary) (primaryRoute int, canaryRoute int, err error) {
 	return 0, 0, nil
+}
+
+func (c *KubernetesRouter) syncService(canary *flaggerv1.Canary, name string, target string) error {
+	portName := canary.Spec.Service.PortName
+	if portName == "" {
+		portName = "http"
+	}
+
+	svcSpec := corev1.ServiceSpec{
+		Type:     corev1.ServiceTypeClusterIP,
+		Selector: map[string]string{"app": target},
+		Ports: []corev1.ServicePort{
+			{
+				Name:     portName,
+				Protocol: corev1.ProtocolTCP,
+				Port:     canary.Spec.Service.Port,
+				TargetPort: intstr.IntOrString{
+					Type:   intstr.Int,
+					IntVal: canary.Spec.Service.Port,
+				},
+			},
+		},
+	}
+
+	svc, err := c.kubeClient.CoreV1().Services(canary.Namespace).Get(name, metav1.GetOptions{})
+	if errors.IsNotFound(err) {
+		svc = &corev1.Service{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: canary.Namespace,
+				OwnerReferences: []metav1.OwnerReference{
+					*metav1.NewControllerRef(canary, schema.GroupVersionKind{
+						Group:   flaggerv1.SchemeGroupVersion.Group,
+						Version: flaggerv1.SchemeGroupVersion.Version,
+						Kind:    flaggerv1.CanaryKind,
+					}),
+				},
+			},
+			Spec: svcSpec,
+		}
+
+		_, err = c.kubeClient.CoreV1().Services(canary.Namespace).Create(svc)
+		if err != nil {
+			return err
+		}
+
+		c.logger.With("canary", fmt.Sprintf("%s.%s", canary.Name, canary.Namespace)).
+			Infof("Service %s.%s created", svc.GetName(), canary.Namespace)
+		return nil
+	}
+
+	if err != nil {
+		return fmt.Errorf("service %s query error %v", name, err)
+	}
+
+	if svc != nil {
+		if diff := cmp.Diff(svcSpec.Ports, svc.Spec.Ports); diff != "" {
+			svcClone := svc.DeepCopy()
+			svcClone.Spec = svcSpec
+			_, err = c.kubeClient.CoreV1().Services(canary.Namespace).Update(svcClone)
+			if err != nil {
+				return fmt.Errorf("service %s update error %v", name, err)
+			}
+			c.logger.With("canary", fmt.Sprintf("%s.%s", canary.Name, canary.Namespace)).
+				Infof("Service %s updated", svc.GetName())
+		}
+	}
+
+	return nil
 }
