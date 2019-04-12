4b17788a77f46e7320b0a9af1f4f1aee3df1525e
stefanprodan <stefan.prodan@gmail.com>
2019-04-12 16:02:23 +0300

    Add Envoy request duration P99 query

--- pkg/metrics/envoy.go
+++ pkg/metrics/envoy.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/url"
 	"strconv"
+	"time"
 )
 
 const envoySuccessRateQuery = `
@@ -62,4 +63,57 @@ func (c *Observer) GetEnvoySuccessRate(name string, namespace string, metric str
 		return 0, fmt.Errorf("no values found for metric %s", metric)
 	}
 	return *rate, nil
+}
+
+const envoyRequestDurationQuery = `
+histogram_quantile(0.99, sum(rate(
+envoy_cluster_upstream_rq_time_bucket{kubernetes_namespace="{{ .Namespace }}",
+kubernetes_pod_name=~"{{ .Name }}-[0-9a-zA-Z]+(-[0-9a-zA-Z]+)"}
+[{{ .Interval }}])) by (le))
+`
+
+// GetEnvoyRequestDuration returns the 99P requests delay using envoy_cluster_upstream_rq_time_bucket metrics
+func (c *Observer) GetEnvoyRequestDuration(name string, namespace string, metric string, interval string) (time.Duration, error) {
+	if c.metricsServer == "fake" {
+		return 1, nil
+	}
+
+	meta := struct {
+		Name      string
+		Namespace string
+		Interval  string
+	}{
+		name,
+		namespace,
+		interval,
+	}
+
+	query, err := render(meta, envoyRequestDurationQuery)
+	if err != nil {
+		return 0, err
+	}
+
+	var rate *float64
+	querySt := url.QueryEscape(query)
+	result, err := c.queryMetric(querySt)
+	if err != nil {
+		return 0, err
+	}
+
+	for _, v := range result.Data.Result {
+		metricValue := v.Value[1]
+		switch metricValue.(type) {
+		case string:
+			f, err := strconv.ParseFloat(metricValue.(string), 64)
+			if err != nil {
+				return 0, err
+			}
+			rate = &f
+		}
+	}
+	if rate == nil {
+		return 0, fmt.Errorf("no values found for metric %s", metric)
+	}
+	ms := time.Duration(int64(*rate*1000)) * time.Millisecond
+	return ms, nil
 }
