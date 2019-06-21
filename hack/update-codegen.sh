6363580120e9d81fc50d4c116d2322d40f17f5cd
stefanprodan <stefan.prodan@gmail.com>
2019-06-21 16:34:19 +0300

    Fix k8s code gen

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -7,7 +7,7 @@ set -o pipefail
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 CODEGEN_PKG=${CODEGEN_PKG:-$(echo `go env GOPATH`'/pkg/mod/k8s.io/code-generator@v0.0.0-20190531131525-17d711082421')}
 
-chmod +x ${CODEGEN_PKG}/generate-groups.sh
+#chmod +x ${CODEGEN_PKG}/generate-groups.sh
 
 ${CODEGEN_PKG}/generate-groups.sh all \
   github.com/weaveworks/flagger/pkg/client github.com/weaveworks/flagger/pkg/apis \
