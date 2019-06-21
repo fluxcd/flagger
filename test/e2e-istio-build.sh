4f0ad7a067c476817c6b38a7533183adb4a96469
stefanprodan <stefan.prodan@gmail.com>
2019-06-21 13:45:16 +0300

    CircleCI - run Istio e2e

--- test/e2e-istio-build.sh
+++ test/e2e-istio-build.sh
@@ -5,9 +5,6 @@ set -o errexit
 REPO_ROOT=$(git rev-parse --show-toplevel)
 export KUBECONFIG="$(kind get kubeconfig-path --name="kind")"
 
-echo '>>> Building Flagger'
-cd ${REPO_ROOT} && docker build -t test/flagger:latest . -f Dockerfile
-
 echo '>>> Load Flagger image in Kind'
 kind load docker-image test/flagger:latest
 
