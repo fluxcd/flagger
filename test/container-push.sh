931c051153408a912d197a0064578ab07187a94a
stefanprodan <stefan.prodan@gmail.com>
2019-06-24 17:46:12 +0300

    Fix tag composition in release script

--- test/container-push.sh
+++ test/container-push.sh
@@ -2,22 +2,25 @@
 
 set -o errexit
 
-BRANCH_COMMIT=${CIRCLE_BRANCH}-$(echo ${CIRCLE_SHA1} | head -c7)
+
+push () {
+    echo $DOCKER_PASS | docker login -u=$DOCKER_USER --password-stdin
+
+    if [[ -z "$CIRCLE_TAG" ]]; then
+        BRANCH_COMMIT=${CIRCLE_BRANCH}-$(echo ${CIRCLE_SHA1} | head -c7);
+        docker tag test/flagger:latest weaveworks/flagger:${BRANCH_COMMIT};
+        docker push weaveworks/flagger:${BRANCH_COMMIT};
+    else
+        docker tag test/flagger:latest weaveworks/flagger:${CIRCLE_TAG};
+        docker tag test/flagger-loadtester:latest weaveworks/flagger-loadtester:${CIRCLE_TAG};
+        docker push weaveworks/flagger:${CIRCLE_TAG};
+        docker push weaveworks/flagger-loadtester:${CIRCLE_TAG};
+    fi
+}
 
 if [[ -z "$DOCKER_PASS" ]]; then
     echo "No Docker Hub credentials, skipping image push";
 else
-    echo $DOCKER_PASS | docker login -u=$DOCKER_USER --password-stdin;
-    docker tag test/flagger:latest weaveworks/flagger:${BRANCH_COMMIT};
-    docker push weaveworks/flagger:${BRANCH_COMMIT};
+    push
 fi
 
-if [[ -z "$CIRCLE_TAG" ]]; then
-    echo "Not a release, skipping image push";
-else
-    echo $DOCKER_PASS | docker login -u=$DOCKER_USER --password-stdin;
-    docker tag test/flagger:latest weaveworks/flagger:${CIRCLE_TAG};
-    docker tag test/flagger-loadtester:latest weaveworks/flagger-loadtester:${CIRCLE_TAG};
-    docker push weaveworks/flagger:${CIRCLE_TAG};
-    docker push weaveworks/flagger-loadtester:${CIRCLE_TAG};
-fi
