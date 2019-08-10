040dbb8d032e48e4d52afd114626f4d69c69a959
stefanprodan <stefan.prodan@gmail.com>
2019-08-10 11:04:15 +0300

    Add http listener to virtual router reconciliation

--- pkg/router/appmesh.go
+++ pkg/router/appmesh.go
@@ -165,6 +165,14 @@ func (ar *AppMeshRouter) reconcileVirtualService(canary *flaggerv1.Canary, name 
 		MeshName: canary.Spec.Service.MeshName,
 		VirtualRouter: &AppmeshV1beta1.VirtualRouter{
 			Name: fmt.Sprintf("%s-router", targetName),
+			Listeners: []AppmeshV1beta1.Listener{
+				{
+					PortMapping: AppmeshV1beta1.PortMapping{
+						Port:     int64(canary.Spec.Service.Port),
+						Protocol: "http",
+					},
+				},
+			},
 		},
 		Routes: []AppmeshV1beta1.Route{
 			{
