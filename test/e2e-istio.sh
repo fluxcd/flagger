c667c947ad2b9b4f6f260698ef7013a39d48e225
stefanprodan <stefan.prodan@gmail.com>
2019-09-22 12:12:06 +0300

    Istio e2e: update job names

--- test/e2e-istio.sh
+++ test/e2e-istio.sh
@@ -13,9 +13,9 @@ echo '>>> Installing Istio CRDs'
 helm upgrade -i istio-init istio.io/istio-init --wait --namespace istio-system
 
 echo '>>> Waiting for Istio CRDs to be ready'
-kubectl -n istio-system wait --for=condition=complete job/istio-init-crd-10
-kubectl -n istio-system wait --for=condition=complete job/istio-init-crd-11
-kubectl -n istio-system wait --for=condition=complete job/istio-init-crd-12
+kubectl -n istio-system wait --for=condition=complete job/istio-init-crd-10-${ISTIO_VER}
+kubectl -n istio-system wait --for=condition=complete job/istio-init-crd-11-${ISTIO_VER}
+kubectl -n istio-system wait --for=condition=complete job/istio-init-crd-12-${ISTIO_VER}
 
 echo '>>> Installing Istio control plane'
 helm upgrade -i istio istio.io/istio --wait --namespace istio-system -f ${REPO_ROOT}/test/e2e-istio-values.yaml
