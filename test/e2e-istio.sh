8d7d5e681060412bff4b60aacef9e06b60909a36
stefanprodan <stefan.prodan@gmail.com>
2020-01-11 20:45:57 +0200

    Update Istio e2e to v1.4.3

--- test/e2e-istio.sh
+++ test/e2e-istio.sh
@@ -2,7 +2,7 @@
 
 set -o errexit
 
-ISTIO_VER="1.3.3"
+ISTIO_VER="1.4.3"
 REPO_ROOT=$(git rev-parse --show-toplevel)
 export KUBECONFIG="$(kind get kubeconfig-path --name="kind")"
 
@@ -15,7 +15,7 @@ helm upgrade -i istio-init istio.io/istio-init --wait --namespace istio-system
 echo '>>> Waiting for Istio CRDs to be ready'
 kubectl -n istio-system wait --for=condition=complete job/istio-init-crd-10-${ISTIO_VER}
 kubectl -n istio-system wait --for=condition=complete job/istio-init-crd-11-${ISTIO_VER}
-kubectl -n istio-system wait --for=condition=complete job/istio-init-crd-12-${ISTIO_VER}
+kubectl -n istio-system wait --for=condition=complete job/istio-init-crd-14-${ISTIO_VER}
 
 echo '>>> Installing Istio control plane'
 helm upgrade -i istio istio.io/istio --wait --namespace istio-system -f ${REPO_ROOT}/test/e2e-istio-values.yaml
