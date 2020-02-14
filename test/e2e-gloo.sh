33d57af233ce1c0b598024c614411fcc3bed7a83
stefanprodan <stefan.prodan@gmail.com>
2020-02-14 12:43:21 +0200

    e2e: Install CRDs with Helm v3

--- test/e2e-gloo.sh
+++ test/e2e-gloo.sh
@@ -21,6 +21,7 @@ kind load docker-image test/flagger:latest
 
 echo '>>> Installing Flagger'
 helm upgrade -i flagger ${REPO_ROOT}/charts/flagger \
+--set crd.create=false \
 --namespace gloo-system \
 --set prometheus.install=true \
 --set meshProvider=gloo
