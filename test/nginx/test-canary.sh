73b7fc1cfc1d3f0034f229898c75ddeb76af6869
Stefan Prodan <stefan.prodan@gmail.com>
2020-12-20 22:21:02 +0200

    Add Traefik e2e tests
    Signed-off-by: Stefan Prodan <stefan.prodan@gmail.com>

--- test/nginx/test-canary.sh
+++ test/nginx/test-canary.sh
@@ -244,7 +244,7 @@ spec:
         timeout: 5s
         metadata:
           type: bash
-          cmd: "curl -sH 'x-user: insider' -H 'Host: app.example.com' http://ingress-nginx-controller.ingress-nginx | grep '3.1.1'"
+          cmd: "curl -sH 'x-user: insider' -H 'Host: app.example.com' http://ingress-nginx-controller.ingress-nginx"
       - name: load-test
         type: rollout
         url: http://flagger-loadtester.test/
---
+++ test/traefik/install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+TRAEFIK_CHART_VERSION="9.11.0" # traefik 2.3.3
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+mkdir -p ${REPO_ROOT}/bin
+
+echo '>>> Creating traefik namespace'
+kubectl create ns traefik
+
+echo '>>> Installing Traefik'
+helm repo add traefik https://helm.traefik.io/traefik
+cat <<EOF | helm upgrade -i traefik traefik/traefik --version=${TRAEFIK_VERSION} --wait --namespace traefik -f -
+additionalArguments:
+  - "--metrics.prometheus=true"
+deployment:
+  podAnnotations:
+    "prometheus.io/port": "9000"
+    "prometheus.io/scrape": "true"
+EOF
+
+kubectl -n traefik get all
+
+echo '>>> Installing Flagger'
+helm upgrade -i flagger ${REPO_ROOT}/charts/flagger \
+--set crd.create=false \
+--namespace traefik \
+--set prometheus.install=true \
+--set meshProvider=traefik \
+--set image.repository=test\/flagger \
+--set image.tag=latest \
+
+kubectl -n traefik rollout status deployment/flagger
+kubectl -n projectcontour rollout status deployment/flagger-prometheus
