fe635a277547e996dc0810a2578ccf93f4b65f71
Keerthan Ekbote <keerthan.ekbote@solo.io>
2021-05-12 14:47:17 -0400

    updated e2es with new upstream ref
    Signed-off-by: Keerthan Ekbote <keerthan.ekbote@solo.io>

--- pkg/apis/gloo/gloo/v1/types.go
+++ pkg/apis/gloo/gloo/v1/types.go
@@ -18,13 +18,13 @@ type Upstream struct {
 
 type UpstreamSpec struct {
 	Kube                        *KubeUpstream         `json:"kube,omitempty"`
-	SslConfig                   *UpstreamSslConfig    `json:"ssl_config,omitempty"`
-	CircuitBreakers             *CircuitBreakerConfig `json:"circuit_breakers,omitempty"`
-	ConnectionConfig            *ConnectionConfig     `json:"connection_config,omitempty"`
-	UseHttp2                    bool                  `json:"use_http2,omitempty"`
-	InitialStreamWindowSize     uint32                `json:"initial_stream_window_size,omitempty"`
-	InitialConnectionWindowSize uint32                `json:"initial_connection_window_size,omitempty"`
-	HttpProxyHostname           string                `json:"http_proxy_hostname,omitempty"`
+	SslConfig                   *UpstreamSslConfig    `json:"sslConfig,omitempty"`
+	CircuitBreakers             *CircuitBreakerConfig `json:"circuitBreakers,omitempty"`
+	ConnectionConfig            *ConnectionConfig     `json:"connectionConfig,omitempty"`
+	UseHttp2                    bool                  `json:"useHttp2,omitempty"`
+	InitialStreamWindowSize     uint32                `json:"initialStreamWindowSize,omitempty"`
+	InitialConnectionWindowSize uint32                `json:"initialConnectionWindowSize,omitempty"`
+	HttpProxyHostname           string                `json:"httpProxyHostName,omitempty"`
 }
 
 type KubeUpstream struct {
@@ -36,9 +36,9 @@ type KubeUpstream struct {
 
 type UpstreamSslConfig struct {
 	Sni                  string         `json:"sni,omitempty"`
-	VerifySubjectAltName []string       `json:"verify_subject_alt_name,omitempty"`
+	VerifySubjectAltName []string       `json:"verifySubjectAltName,omitempty"`
 	Parameters           *SslParameters `json:"parameters,omitempty"`
-	AlpnProtocols        []string       `json:"alpn_protocols,omitempty"`
+	AlpnProtocols        []string       `json:"alpnProtocols,omitempty"`
 
 	/** SSLSecrets -- only one of these should be set */
 	*UpstreamSslConfig_Sds      `json:"sds,omitempty"`
@@ -48,64 +48,64 @@ type UpstreamSslConfig struct {
 
 // SSLFiles reference paths to certificates which can be read by the proxy off of its local filesystem
 type UpstreamSslConfig_SslFiles struct {
-	TlsCert string `json:"tls_cert,omitempty"`
-	TlsKey  string `json:"tls_key,omitempty"`
-	RootCa  string `json:"root_ca,omitempty"`
+	TlsCert string `json:"tlsCert,omitempty"`
+	TlsKey  string `json:"tlsKey,omitempty"`
+	RootCa  string `json:"rootCa,omitempty"`
 }
 
 // Use secret discovery service.
 type UpstreamSslConfig_Sds struct {
-	TargetUri              string `json:"target_uri,omitempty"`
-	CertificatesSecretName string `json:"certificates_secret_name,omitempty"`
-	ValidationContextName  string `json:"validation_context_name,omitempty"`
+	TargetUri              string `json:"targetUri,omitempty"`
+	CertificatesSecretName string `json:"certificatesSecretName,omitempty"`
+	ValidationContextName  string `json:"validationContextName,omitempty"`
 
 	/** SDSBuilder -- onle one of the following can be set */
-	CallCredentials *CallCredentials `json:"call_credentials,omitempty"`
-	ClusterName     string           `json:"cluster_name,omitempty"`
+	CallCredentials *CallCredentials `json:"callCredentials,omitempty"`
+	ClusterName     string           `json:"clusterName,omitempty"`
 }
 
 type CallCredentials struct {
-	FileCredentialSource *CallCredentials_FileCredentialSource `json:"file_credential_source,omitempty"`
+	FileCredentialSource *CallCredentials_FileCredentialSource `json:"fileCredentialSource,omitempty"`
 }
 
 type CallCredentials_FileCredentialSource struct {
-	TokenFileName string `json:"token_file_name,omitempty"`
+	TokenFileName string `json:"tokenFileName,omitempty"`
 	Header        string `json:"header,omitempty"`
 }
 
 type SslParameters struct {
-	MinimumProtocolVersion int32    `json:"minimum_protocol_version,omitempty"`
-	MaximumProtocolVersion int32    `json:"maximum_protocol_version,omitempty"`
-	CipherSuites           []string `json:"cipher_suites,omitempty"`
-	EcdhCurves             []string `json:"ecdh_curves,omitempty"`
+	MinimumProtocolVersion int32    `json:"minimumProtocolVersion,omitempty"`
+	MaximumProtocolVersion int32    `json:"maximumProtocolVersion,omitempty"`
+	CipherSuites           []string `json:"cipherSuites,omitempty"`
+	EcdhCurves             []string `json:"ecdhCurves,omitempty"`
 }
 
 type CircuitBreakerConfig struct {
-	MaxConnections     uint32 `json:"max_connections,omitempty"`
-	MaxPendingRequests uint32 `json:"max_pending_requests,omitempty"`
-	MaxRequests        uint32 `json:"max_requests,omitempty"`
-	MaxRetries         uint32 `json:"max_retries,omitempty"`
+	MaxConnections     uint32 `json:"maxConnections,omitempty"`
+	MaxPendingRequests uint32 `json:"maxPendingRequests,omitempty"`
+	MaxRequests        uint32 `json:"maxRequests,omitempty"`
+	MaxRetries         uint32 `json:"maxRetries,omitempty"`
 }
 
 type ConnectionConfig struct {
-	MaxRequestsPerConnection      uint32                                `json:"max_requests_per_connection,omitempty"`
-	ConnectTimeout                *Duration                             `json:"connect_timeout,omitempty"`
-	TcpKeepalive                  *ConnectionConfig_TcpKeepAlive        `json:"tcp_keepalive,omitempty"`
-	PerConnectionBufferLimitBytes uint32                                `json:"per_connection_buffer_limit_bytes,omitempty"`
-	CommonHttpProtocolOptions     *ConnectionConfig_HttpProtocolOptions `json:"common_http_protocol_options,omitempty"`
+	MaxRequestsPerConnection      uint32                                `json:"maxRequestsPerConnection,omitempty"`
+	ConnectTimeout                *Duration                             `json:"connectTimeout,omitempty"`
+	TcpKeepalive                  *ConnectionConfig_TcpKeepAlive        `json:"tcpKeepalive,omitempty"`
+	PerConnectionBufferLimitBytes uint32                                `json:"perConnectionBufferLimitBytes,omitempty"`
+	CommonHttpProtocolOptions     *ConnectionConfig_HttpProtocolOptions `json:"commonHttpProtocolOptions,omitempty"`
 }
 
 type ConnectionConfig_TcpKeepAlive struct {
-	KeepaliveProbes   uint32    `json:"keepalive_probes,omitempty"`
-	KeepaliveTime     *Duration `json:"keepalive_time,omitempty"`
-	KeepaliveInterval *Duration `json:"keepalive_interval,omitempty"`
+	KeepaliveProbes   uint32    `json:"keepaliveProbes,omitempty"`
+	KeepaliveTime     *Duration `json:"keepaliveTime,omitempty"`
+	KeepaliveInterval *Duration `json:"keepaliveInterval,omitempty"`
 }
 
 type ConnectionConfig_HttpProtocolOptions struct {
-	IdleTimeout                  *Duration `json:"idle_timeout,omitempty"`
-	MaxHeadersCount              uint32    `json:"max_headers_count,omitempty"`
-	MaxStreamDuration            *Duration `json:"max_stream_duration,omitempty"`
-	HeadersWithUnderscoresAction uint32    `json:"headers_with_underscores_action,omitempty"`
+	IdleTimeout                  *Duration `json:"idleTimeout,omitempty"`
+	MaxHeadersCount              uint32    `json:"maxHeadersCount,omitempty"`
+	MaxStreamDuration            *Duration `json:"maxStreamDuration,omitempty"`
+	HeadersWithUnderscoresAction uint32    `json:"headersWithUnderscoresAction,omitempty"`
 }
 
 type Duration struct {
