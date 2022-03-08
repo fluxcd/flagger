5acf189fbe8b57183314bf5ea9eceb9b0d3f9d77
Sanskar Jaiswal <sanskar.jaiswal@weave.works>
2022-03-08 19:29:00 +0530

    remove support for helmv2 in loadtester
    Signed-off-by: Sanskar Jaiswal <sanskar.jaiswal@weave.works>

--- cmd/loadtester/main.go
+++ cmd/loadtester/main.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 The Flux authors
+Copyright 2020, 2022 The Flux authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -28,7 +28,7 @@ import (
 	"go.uber.org/zap"
 )
 
-var VERSION = "0.21.0"
+var VERSION = "0.22.0"
 var (
 	logLevel          string
 	port              string
