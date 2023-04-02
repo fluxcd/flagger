49d59f3b45472c7ea01fec8b761b291e3688ff65
John Harris <john@johnharris.io>
2023-04-02 07:17:39 -0700

    update Kuma version and docs
    Signed-off-by: John Harris <john@johnharris.io>

--- test/kuma/install.sh
+++ test/kuma/install.sh
@@ -2,9 +2,8 @@
 
 set -o errexit
 
-KUMA_VER="1.6.0"
+KUMA_VER="2.1.0"
 REPO_ROOT=$(git rev-parse --show-toplevel)
-KUSTOMIZE_VERSION=3.8.2
 mkdir -p ${REPO_ROOT}/bin
 
 echo ">>> Downloading Kuma ${KUMA_VER}"
@@ -20,46 +19,12 @@ echo ">>> Waiting for Kuma Control Plane to be ready"
 kubectl wait --for condition=established crd/meshes.kuma.io
 kubectl -n kuma-system rollout status deployment/kuma-control-plane
 
-echo ">>> Configuring Default Kuma Mesh"
-cat <<EOF | kubectl apply -f -
-apiVersion: kuma.io/v1alpha1
-kind: Mesh
-metadata:
-  name: default
-spec:
-  metrics:
-    enabledBackend: prometheus-1
-    backends:
-      - name: prometheus-1
-        type: prometheus
-        conf:
-          skipMTLS: true
-          port: 5670
-          path: /metrics
-          tags:
-            kuma.io/service: dataplane-metrics
-  mtls:
-    enabledBackend: ca-1
-    backends:
-      - name: ca-1
-        type: builtin
-        mode: PERMISSIVE
-        dpCert:
-          rotation:
-            expiration: 1d
-        conf:
-          caCert:
-            RSAbits: 2048
-            expiration: 10y
-EOF
-
-echo '>>> Installing Kustomize'
-cd ${REPO_ROOT}/bin && kustomize_url=https://github.com/kubernetes-sigs/kustomize/releases/download && \
-curl -sL ${kustomize_url}/kustomize%2Fv${KUSTOMIZE_VERSION}/kustomize_v${KUSTOMIZE_VERSION}_linux_amd64.tar.gz | \
-tar xz
+echo ">>> Installing Prometheus"
+${REPO_ROOT}/bin/kumactl install observability --components "prometheus" | kubectl apply -f -
+kubectl -n mesh-observability rollout status deployment/prometheus-server
 
 echo '>>> Installing Flagger'
-${REPO_ROOT}/bin/kustomize build ${REPO_ROOT}/test/kuma | kubectl apply -f -
+kubectl apply -k ${REPO_ROOT}/kustomize/kuma
 
 kubectl -n kuma-system set image deployment/flagger flagger=test/flagger:latest
 kubectl -n kuma-system rollout status deployment/flagger
